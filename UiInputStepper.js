c2288d29a78b0bacc354a10186a09c8377bfd3ac
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-21 22:00:52 +0900

    first

---
+++ UiInputStepper.js
@@ -0,0 +1,151 @@
+class UiInputStepper{
+  /**
+   * setTimeout delay at first
+   *
+   * @static
+   * @type {number}
+   */
+  static firstDelay = 200 // ms
+    
+  /**
+   * delay multipler
+   * 1 does not change the delay time.
+   *
+   * @static
+   * @type {number}
+   */
+  static delayMultipler = 0.98
+
+  /**
+   * setTimeout min delay
+   *
+   * @static
+   * @type {number}
+   */
+  static minDelay = 5; // ms
+
+  /**
+   * 이벤트 등록
+   *
+   * @static
+   * @param {HTMLElement} [target=window] 
+   */
+  static addEventListener(target=window){
+    target.addEventListener('pointerdown',this.onpointerdown)
+    target.addEventListener('input',this.oninput)
+  }
+
+  /**
+   * 이벤트 제거
+   *
+   * @static
+   * @param {HTMLElement} [target=window] 
+   */
+  static removeEventListener(target=window){
+    target.removeEventListener('pointerdown',this.onpointerdown)
+    target.removeEventListener('input',this.oninput)
+  }
+
+  /**
+   * 단계값 증가
+   *
+   * @static
+   * @param {HTMLInputElement} input 
+   * @param {string} stepper step type up/down/none
+   * @param {Event} [event=null] relative event
+   */
+  static step(input,stepper,event=null){
+    switch(stepper){
+      case 'up':input.stepUp();break;
+      case 'down':input.stepDown();break;
+      case 'none':break;
+      default: throw new Error(`Unsupported stepper. (${stepper})`);
+    }
+    const wrap = input.closest('.ui-input-stepper');
+    if(wrap.classList.contains('data-value')){
+      if(wrap.oninput){
+        if(event===null || event.type!='input'){ 
+          const fakeEvent = {
+            type: 'input',
+            target: input,
+            timeStamp: Date.now(),
+          }
+          wrap.oninput(fakeEvent) 
+        }
+      }else{
+        wrap.dataset.value = input.value;
+      }
+    }
+  }
+  static currentDelay = 200;
+  static tm = null;
+  static setTimeout(input,stepper,event){
+    this.tm = setTimeout(() => {
+      this.step(input,stepper,event);
+      this.setTimeout(input,stepper,event);
+    }, this.currentDelay);
+    
+    if(this.currentDelay > this.minDelay){
+      this.currentDelay = Math.max(this.minDelay,this.currentDelay * this.delayMultipler);   
+    }
+    
+  } 
+  
+  /**
+   * onpointerdown process method
+   *
+   * @param {Event} event 
+   */
+  static onpointerdown = (event)=>{
+    const target = event.target;
+    const wrap = target.closest('.ui-input-stepper')
+    if(!wrap){ return;}
+    const input = wrap.querySelector('input');
+    if(!input){ return; }
+    if(!target.dataset.stepper){ return; }
+    const stepper = target.dataset.stepper
+    
+    this.step(input,stepper,event)
+    this.currentDelay = this.firstDelay;
+    this.setTimeout(input,stepper,event)
+    window.addEventListener('pointerup',this.onpointerup,{once:true});
+  }
+  
+  /**
+   * onpointerup process method
+   *
+   * @param {Event} event 
+   */
+  static onpointerup = (event)=>{
+    if(this.tm){clearTimeout(this.tm);}
+  }
+
+  /**
+   * oninput process method
+   *
+   * @param {Event} event 
+   */
+  static oninput = (event)=>{
+    const target = event.target;
+    const wrap = target.closest('.ui-input-stepper')
+    if(!wrap){ return;}
+    const input = wrap.querySelector('input');
+    if(!input){ return; }
+    this.step(input,'none',null)
+  }
+
+
+  /**
+   * initialize data-value attribute
+   *
+   * @static
+   */
+  static initDataValue(){
+    window.document.querySelectorAll('.ui-input-stepper input').forEach((input)=>{
+      console.log(input);
+      this.step(input,'none',null)
+    })
+  }
+
+  
+}
