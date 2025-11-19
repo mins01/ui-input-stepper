4ef748a7891880cfff604b8e9065db9969c9bce6
mins01 <mins01@inuscomm.co.kr>
2025-11-19 13:45:35 +0900

    formatted-output 애트리뷰트

--- UiInputStepperWrapper.js
+++ UiInputStepperWrapper.js
@@ -3,7 +3,7 @@ class UiInputStepperWrapper extends HTMLElement  {
   static firstDelay = 200; // 시작 딜레이
   static delayMultiplier = 0.96 // 변화량
   static minDelay = 5; //최소 딜레이
-  static maxDelay = 1000; //최소 딜레이
+  static maxDelay = 1000; //최대 딜레이
 
   #tmId = null;
   #valueInput = null;
@@ -19,11 +19,12 @@ class UiInputStepperWrapper extends HTMLElement  {
     this.#valueInput = null;
     this.#valueOutput = null;
 
-    this.sync();
+    this.syncValue();
 
     this.addEventListener('pointerdown',this.pointerdownHandler)
     this.addEventListener('pointerup',this.pointerupHandler)
-    this.addEventListener('input',()=>{ this.sync() })
+    this.addEventListener('pointercancel',this.pointerupHandler)
+    this.addEventListener('input',()=>{ this.syncValue() })
   }
 
   static defineCustomElement(name='ui-input-stepper-wrapper'){
@@ -32,9 +33,10 @@ class UiInputStepperWrapper extends HTMLElement  {
       console.debug('defineCustomElement',name);
   }
 
-  // connectedCallback() {
-  //  console.log("<div is='ui-input-stepper-wrapper'> attached.");
-  // }
+  connectedCallback() {
+    //  console.log("<div is='ui-input-stepper-wrapper'> attached.");
+    this.syncValue();
+  }
   pointerdownHandler(event){
     const target = event.target;
     this.clearValueElements();
@@ -49,7 +51,7 @@ class UiInputStepperWrapper extends HTMLElement  {
       this.setPointerCapture(event.pointerId); // 포인터 캡처 시작
       this.stopRepeat()
       this.delay = this.firstDelay;
-      const stepIncrement = parseInt(target.dataset?.stepIncrement??'1');
+      const stepIncrement = parseInt(target.dataset?.stepIncrement??'1',10);
       this.repeatStepDown(stepIncrement)
     }
   }
@@ -86,7 +88,7 @@ class UiInputStepperWrapper extends HTMLElement  {
   }
   set value(v){
     if(this?.valueInput) this.valueInput.value = v;
-    this.sync();
+    this.syncValue();
   }
 
 
@@ -105,17 +107,17 @@ class UiInputStepperWrapper extends HTMLElement  {
   }
   stepUp(v){ 
     this?.valueInput?.stepUp(v); 
-    this.sync();
+    this.syncValue();
   }
   stepDown(v){ 
     this?.valueInput?.stepDown(v); 
-    this.sync();
+    this.syncValue();
   }
-  sync(){
-    this.syncOutput();
+  syncValue(){
+    this.syncValueOutput();
   }
-  syncOutput(){
-    if(this.valueOutput) this.valueOutput.textContent = this.formattedOutput(this?.valueInput?.value);
+  syncValueOutput(){
+    if(this.valueOutput) this.valueOutput.textContent = this.formattedOutput(this.value);
   }
 
   // 이거 재선언해서 사용하자.
@@ -141,6 +143,6 @@ class UiInputStepperWrapper extends HTMLElement  {
     else if (name === "delay-multiplier") { this.delayMultiplier = parseFloat(newValue); }
     else if (name === "min-delay") { this.minDelay = parseFloat(newValue); }
     else if (name === "max-delay") { this.maxDelay = parseFloat(newValue); }
-    else if (name === "formatted-output") { this.formattedOutput = new Function('value', newValue); this.sync(); }
+    else if (name === "formatted-output") { this.formattedOutput = (value)=>newValue.replace('{value}',value) }
   }
 }
